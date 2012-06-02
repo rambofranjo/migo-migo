2f00075e650d26bb905eda41e04b218bf7341882
markovicb@gmx.at <markovicb@gmx.at@ddc55a17-9788-0436-19d6-acb432304061>
2012-06-02 15:57:03 +0000

    Termine hinzufügen (fkt)

--- apps/migo/Root/newAppointment.js
+++ apps/migo/Root/newAppointment.js
@@ -21,9 +21,19 @@ function newAppointment_action() {
 		/** split time info */
 		time1 = startzeit.toString().split(":");
 		time2 = endzeit.toString().split(":");
-
 		
-		if ((time1 != "") && (time2 != "") && (ort != "")) {
+		//res.write(time1);
+		//res.write(time2);
+		
+		re = /^\d{1,2}:\d{2}([ap]m)?$/;
+		
+		if(!startzeit.match(re) || !endzeit.match(re)) {
+			error = "Zeitangabe muss die Form HH:MM haben!";
+		} else if (ort == "") {
+			error = "Terminort muss angegeben werden!";
+		}
+		
+		if (startzeit.match(re) && endzeit.match(re) && (ort != "")) {
 			
 			
 			
@@ -38,7 +48,7 @@ function newAppointment_action() {
 			newAppointment.beschreibung = beschreibung;
 			newAppointment.tag = "findet statt";
 			newAppointment.datum = new Date(datum);
-			newAppointment.startzeit = new Date(2012, 2, 2, time1[0], time1[1],0, 0);
+			newAppointment.startzeit = new Date(2012, 02, 02, time1[0], time1[1],00, 00);
 			newAppointment.endzeit = new Date(2012, 02, 02, time2[0], time2[1], 00, 00);
 			//res.writeln("1");
 			newAppointment.ort = ort;
@@ -46,17 +56,22 @@ function newAppointment_action() {
 			newAppointment.personID = session.user._id;
 			root.termine.add(newAppointment);
 			
-			//res.redirect(root.href("groupNews") + "?groupId=" + session.data.grpId);
 			res.redirect(root.href("newAppointment") + "?groupId=" + session.data.grpId);
 		} else {	
-			res.data.errorNewAppointment = "Fehler!";
-			/*
-			res.data.newAppointmentDate = datum;
-			res.data.starttime = time1;
-			res.data.endtime = time2;
-			res.data.newAppointmentPlace = place;
-			res.data.newAppointmentText = text;
-			*/
+			res.data.errorNewAppointment = "Fehler: " + error;
+			
+			if(titel == "Training") res.data.selectTraining = "selected";
+			if(titel == "Spiel") res.data.selectSpiel = "selected";
+			else res.data.selectSonstiges = "selected";
+			
+			res.data.beschreibung = beschreibung;
+			res.data.tag = "findet statt";
+			
+			//res.data.datum = new Date(datum);
+			//res.data.startzeit = new Date(2012, 2, 2, time1[0], time1[1],0, 0);
+			//res.data.endzeit = endzeit = new Date(2012, 02, 02, time2[0], time2[1], 00, 00);
+			
+			res.data.ort = ort;
 			root.renderGruppe("appointmentError");
 		}
 		
