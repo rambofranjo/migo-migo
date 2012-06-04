46dec01a2c5c067f54dcca4c62bd89aed7d6535e
markovicb@gmx.at <markovicb@gmx.at@ddc55a17-9788-0436-19d6-acb432304061>
2012-06-04 12:11:25 +0000

    Termine löschen + bearbeiten

--- apps/migo/Root/editAppointment.js
+++ apps/migo/Root/editAppointment.js
@@ -15,33 +15,39 @@ function editAppointment_action() {
 				/* --- save button gedrückt -- */
 				if (req.data.edit) {
 					
-					/*
-					//News bearbeiten
-					var title = req.data.title;
-					var text = req.data.text;
+					//Appointments bearbeiten
+					var aOrt = req.data.aOrt;
+					var aText = req.data.aText;
+					var startzeit = req.data.startzeit;
+					var endzeit = req.data.endzeit; 
+					var datum = req.data.datum;
+					
 					
 					//Überprüfungen
-					if ((title != "") && (text != "")) {
-						a.titel = title;
-						a.text = text;
+					if ((aOrt != "") && (startzeit != "") && (endzeit != "") && (datum != "")) {
+						// in DB schreiben 
+						a.ort = aOrt;
+						a.beschreibung = aText;
+						a.startzeit = startzeit;
+						a.endzeit = endzeit;
+						a.datum = datum;
 						
 						//Gruppe Appointment rendern
 						root.renderGruppe("appointment");
 						
-						//Zurück zu News leiten
+						//Zurück zu Appointment leiten
 						res.redirect(root.href("groupAppointment") + "?groupId=" + session.data.grpId);	
 					} else {
 						res.data.errorEditAppointment = "Fehler aufgetreten!"
-						res.data.nTitle = title;
-						res.data.nText = text;
-						res.data.nId = nId;
+						res.data.aOrt = aOrt;
+						res.data.aText = aText;
+						res.data.startzeit = startzeit;
 						res.data.grpId = grpId;
 						
-						//Gruppeninfos mit edit News rendern
+						//Gruppeninfos mit edit Appointment rendern
 						root.renderGruppe("editAppointment");
 					}
 					
-					*/
 				
 				} else if (req.data.cancel) {
 					res.redirect(root.href("groupAppointment") + "?groupId=" + session.data.grpId);
@@ -51,8 +57,10 @@ function editAppointment_action() {
 					//Daten im Formular setzen
 					res.data.aOrt = a.ort;
 					res.data.aText = a.beschreibung;
-					
-					
+					res.data.startzeit = a.startzeit.getHours() + ":" + a.startzeit.getMinutes();
+					res.data.endzeit = a.endzeit.getHours() + ":" + a.endzeit.getMinutes(); 
+					//res.data.datum = a.datum.getDay() + "/" + a.datum.getMonth() + "/" + a.datum.getYear();
+
 					res.data.aId = aId;
 					res.data.grpId = grpId;
 					
